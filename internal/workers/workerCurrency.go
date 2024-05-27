9b8c979d011412a446ac75379ca5619e48da8afa
Gondir <ya.bashkirov2002@yandex.ru>
2024-05-27 14:18:27 +0400

    zxc

--- internal/workers/workerCurrency.go
+++ internal/workers/workerCurrency.go
@@ -18,32 +18,32 @@ func InitWorkerCurrency(log *zap.Logger, valuteStor storage.CurrencyStorage) {
 	c := cron.New(cron.WithSeconds())
 	logger = log
 	currStr = valuteStor
-
-	_, err := c.AddFunc("0 0 10 * * ?", WriteInfoToDb)
+	log.Info("workerCurrency init")
+	_, err := c.AddFunc("0 0 10 * * ?", writeInfoToDb)
 	if err != nil {
 		log.Fatal("could not schedule task:", zap.Error(err))
 	}
 	c.Start()
 }
 
-func WriteInfoToDb() {
+func writeInfoToDb() {
 
-	ctx := context.TODO()
+	ctx := context.Background()
 
-	select {
-	case <-ctx.Done():
-	default:
-		currencies, err := currencyapi.GetCurrencyByToday()
-		if err != nil {
-			logger.Error("can not get api", zap.Error(err))
-		}
-
-		for _, currency := range currencies {
-			err = currStr.InsertCurrency(ctx, currency.NameCurrency, currency.Value)
-			if err != nil {
-				logger.Error("can not write to db", zap.Error(err))
-			}
-		}
+	// select {
+	// case <-ctx.Done():
+	// default:
+	currencies, err := currencyapi.GetCurrencyByToday()
+	if err != nil {
+		logger.Error("can not get api", zap.Error(err))
 	}
 
+	for _, currency := range currencies {
+		err = currStr.InsertCurrency(ctx, currency.NameCurrency, currency.Value)
+		if err != nil {
+			logger.Error("can not write to db", zap.Error(err))
+		}
+	}
+	logger.Info("make the work")
+	// }
 }
